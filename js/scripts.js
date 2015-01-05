5344a6e438c041ced5d44a4533ec0bcf18fba111
Jason Halsey <jason@halsey-design.com>
2015-01-05 14:31:28 -0800

    add in-page navigation js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,68 @@
-$( document ).ready(function() {
-	var biggestHeight = "0";
+
+//Set height of content-stripe based on height of img 
+function contentStripeAdjust() {
+  var biggestHeight = "0";
 	$(".content-stripe *").each(function(){
 	 if ($(this).height() > biggestHeight ) {
 	   biggestHeight = $(this).height()
 	 }
 	});
-
 	$(".content-stripe").height(biggestHeight);
+}
+
+
+
+
+
+
+$( document ).ready(function() {
+
+	$('section.content-stripe').inViewport(
+	    function(){$(this).addClass("current");},
+	    function(){$(this).removeClass("current");}
+	);
+	
+	contentStripeAdjust()
+
+	 $('section.content-stripe').first();
+
+	$('a.display').on('click', function(e) {
+	    e.preventDefault();
+
+	      var t = $(this).text(),
+	      that = $(this);
+
+	    if (t === 'next' && $('.current').next('section.content-stripe').length > 0) {
+	        var $next = $('.current').next('.content-stripe');
+	        var top = $next.offset().top;
+	        
+	        $('.current').removeClass('current');
+	      
+	        $('body').animate({
+	          scrollTop: top     
+	        }, function () {
+	               $next.addClass('current');
+	        });
+	  } else if (t === 'prev' && $('.current').prev('section.content-stripe').length > 0) {
+	        var $prev = $('.current').prev('.content-stripe');
+	        var top = $prev.offset().top;
+	        
+	        $('.current').removeClass('current');
+	      
+	        $('body').animate({
+	          scrollTop: top     
+	        }, function () {
+	               $prev.addClass('current');
+	        });
+	  } 
+	});
+
+	
 });
 
 
 $(window).resize(function() {
-	var biggestHeight = "0";
-	$(".content-stripe *").each(function(){
-	 if ($(this).height() > biggestHeight ) {
-	   biggestHeight = $(this).height()
-	 }
-	});
-
-	$(".content-stripe").height(biggestHeight);
+	
+	contentStripeAdjust()
 
 });
