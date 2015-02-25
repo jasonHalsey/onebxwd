859b181258e8ce07f2ed522c7be1ddc09912d443
Jason Halsey <jason@halsey-design.com>
2015-02-25 09:58:32 -0800

    add onebxwd credentials to instagram feed

--- js/hd-spectragram.js
+++ js/hd-spectragram.js
@@ -4,23 +4,16 @@
 $( document ).ready(function() {
 
 
-jQuery.fn.spectragram.accessData = {
-    accessToken: '5901508.b9888bd.82aa912f348e47c6b515c83757b0536c',
-    clientID: 'b9888bd34df24f36a40fa65b876c72c9'
-};
+	jQuery.fn.spectragram.accessData = {
+	    accessToken: '1517312917.78d68d2.4bafa9ebfcf74d7b8d46d3ab9c417515',
+	    clientID: '78d68d2abad5427787753951f5e11376'
+	};
 
-// $('div.jhalzfeed').spectragram({
-//     query: 'jhalz',
-//     max: 14,
-//     size: 'big',
-//     wrapEachWith: '<p></p>'
-// });
-
-$('div.jhalzfeed').spectragram('getUserFeed',{
-    query: 'jhalz',
-    max: 14,
-    size: 'big',
-    wrapEachWith: '<p></p>'
-});
+	$('ul.jhalzfeed').spectragram('getUserFeed',{
+	    query: 'onebxwd',
+	    max: 32,
+	    size: 'big',
+	    wrapEachWith: '<li class="instagram_image"></li>'
+	});
 
 });
